fdc96c61c84ba11717659b85a8416845e9f5f95d
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-02-29 17:06:19 -0600

    router file setup

---
+++ src/router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./layout/Root";
+import Home from "./layout/Home";
+import BookDetails from "./features/books/BookDetails";
+import AuthForm from "./features/auth/AuthForm";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/books", element: <Home /> },
+      { path: "/books/:id", element: <BookDetails /> },
+      /*{ path: "/login", element: <AuthForm /> },*/
+    ],
+  },
+]);
+
+export default router;
