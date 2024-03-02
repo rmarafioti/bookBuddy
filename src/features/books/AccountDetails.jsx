22e4a429ab391dfd3b90d6fcb21ec3110f311278
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-01 22:56:00 -0600

    cleaned up AccountDetails and ReservedBooks files

--- src/features/books/AccountDetails.jsx
+++ src/features/books/AccountDetails.jsx
@@ -1,24 +1,19 @@
 import React from "react";
 import { useCheckoutListQuery } from "./bookSlice";
 import ReservedBooks from "./ReservedBooks";
+import { useGetUserQuery } from "../auth/authSlice";
 
 export default function AccountDetails() {
-  const { data: reservations, refetch } = useCheckoutListQuery();
-
-  const handleDeletionSuccess = () => {
-    refetch();
-  };
+  const { data: regUser } = useGetUserQuery();
+  const { data: reservations } = useCheckoutListQuery();
 
   return (
     <>
       <h1>Account Details</h1>
+      <p>EMAIL: {regUser?.email}</p>
       {reservations ? (
         reservations.map((reservation) => (
-          <ReservedBooks
-            key={reservation.id}
-            reservation={reservation}
-            onDeletionSuccess={handleDeletionSuccess}
-          />
+          <ReservedBooks key={reservation.id} reservation={reservation} />
         ))
       ) : (
         <p>Nothing Reserved</p>
--- src/features/books/ReservedBooks.jsx
+++ src/features/books/ReservedBooks.jsx
@@ -1,28 +1,18 @@
 import { useDeleteReservationMutation } from "./bookSlice";
 
-export default function ReservedBooks({ reservation, onDeletionSuccess }) {
-  const [deleteReservation, { isLoading, isSuccess }] =
-    useDeleteReservationMutation();
+export default function ReservedBooks({ reservation }) {
+  const [deleteReservation] = useDeleteReservationMutation();
 
   const resDelete = async (e) => {
     e.preventDefault();
-    try {
-      await deleteReservation(reservation.id).unwrap();
-      // Call the onDeletionSuccess callback if provided
-      if (onDeletionSuccess) onDeletionSuccess();
-    } catch (error) {
-      console.error("Error deleting reservation:", error);
-      // Handle the error appropriately
-    }
+    deleteReservation(reservation.id);
   };
 
   return (
     <li>
       <h3>{reservation.title}</h3>
       <h4>{reservation.author}</h4>
-      <button onClick={resDelete} disabled={isLoading}>
-        {isLoading ? "Processing..." : "Return Book"}
-      </button>
+      <button onClick={resDelete}>Return Book</button>
     </li>
   );
 }
