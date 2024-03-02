292129b18b6efaaeaf51cf941821a274768ec0c2
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-01 20:04:12 -0600

    cleaned up/added comments/added getBook endpoint

--- src/features/books/bookSlice.js
+++ src/features/books/bookSlice.js
@@ -2,7 +2,7 @@
  *GET books
  *checkout book: use patch
  *return book: use DELETE
- *look at documnetation for transform response nested object elements
+ *look at documentation for transform response nested object elements
  *look at documentation for transform error response "
  */
 
@@ -10,21 +10,37 @@ import api from "../../store/api";
 
 const bookApi = api.injectEndpoints({
   endpoints: (builder) => ({
+    //returns a list of all the books
     getBooks: builder.query({
       query: () => "/books",
       providesTags: ["books"],
-      transformResponse: (response) => response.books /*could be data.books*/,
+      transformResponse: (response) => response.books,
       transformErrorResponse: (response) => response.data.error.message,
     }),
-    checkoutBook: builder.mutation({
-      query: ({ id, available }) => ({
-        url: `/books/${id}`,
+    //returns the detials of a single book
+    getBook: builder.query({
+      query: (bookid) => `/books/${id}`,
+      providesTags: ["books"],
+      transformResponse: (response) => response.books,
+      transformErrorResponse: (response) => response.data.error.message,
+    }),
+    //returns a list of books the registered user has checked out
+    checkoutList: builder.query({
+      query: () => "/reservations/",
+      provideTags: ["books"],
+      transformResponse: (response) => response.reservation,
+    }),
+    //registered user can checkout or return book with a valid token
+    checkoutOrReturnBook: builder.mutation({
+      query: ({ bookid, available }) => ({
+        url: "/books/" + bookid,
         method: "PATCH",
         body: available,
       }),
       invalidateTags: ["books"],
       transformErrorResponse: (response) => response.data.message,
     }),
+    //return checked out book
     deleteReservation: builder.mutation({
       query: (reservationid) => ({
         url: "/reservations/" + reservationid,
@@ -33,17 +49,12 @@ const bookApi = api.injectEndpoints({
       invalidateTags: ["books"],
       transformResponse: (response) => response.book,
     }),
-    checkoutList: builder.query({
-      query: () => "/reservations/",
-      provideTags: ["books"],
-      transformResponse: (response) => response.reservation,
-    }),
   }),
 });
 
 export const {
   useGetBooksQuery,
-  useCheckoutBookMutation,
+  useCheckoutOrReturnBookMutation,
   useDeleteReservationMutation,
   useCheckoutListQuery,
 } = bookApi;
