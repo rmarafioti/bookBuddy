c519e157b5a175d6773fd150f54741b4b0da6b38
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-02 18:00:54 -0600

    finished styling/color palette main page

--- src/features/books/BookList.jsx
+++ src/features/books/BookList.jsx
@@ -7,8 +7,9 @@ import { Link } from "react-router-dom";
 function BookCard({ book }) {
   return (
     <li id="books">
-      <p>{book.title}</p>
-      <h3>{book.author}</h3>
+      <h2 id="bookTitle">{book.title}</h2>
+      <h5>{book.author}</h5>
+      <img id="bookImage" src={book.coverimage} alt="Book cover" />
       <Link id="seeDetails" to={`/books/${book.id}`}>
         See Details
       </Link>
