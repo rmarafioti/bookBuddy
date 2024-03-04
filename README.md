a76bb6d259357bd7494fd2fc8f9033c6459269b5
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-04 07:32:42 -0600

    added search bar w some styling

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ I started this project by creating a wireframe for the ui which I then broke dow
 
 After the initial set up of files and path functionality I ran into some trouble with my code. Using source code, documentation and some help from chapGPT I ended up with an app that functioned 90% but was not refetching by utilizing 'provideTags' and "invalidatesTag'. This made it so I had to refresh the page when I changed view or deleted a reservation. My intuition was that I had too much code that was not doing what it needed to do properly.
 
-To not start over from scratch I created a feature branch in git and worked to correct the issues. In doing so I noticed that pairing down child components I had declared in my wireframe was also helpful in keeping things more organized. I understanded pivoting from my initial outline is not be the best practice but because I am still learning and the only one working on the project I decided to do so.
+To not start over from scratch & show transparency I created a feature branch in git and worked to correct the issues. In doing so I noticed that pairing down child components I had declared in my wireframe was also helpful in keeping things more organized. I understanded pivoting from my initial outline is not be the best practice but because I am still learning and the only one working on the project I decided to do so.
 
 Once all the changes were made I merged my branch back to main and moved forward with styling.
 
--- src/features/books/BookList.jsx
+++ src/features/books/BookList.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { useGetBooksQuery } from "./bookSlice";
 import { Link } from "react-router-dom";
 
@@ -21,13 +22,55 @@ function BookCard({ book }) {
 
 export default function BookList() {
   const { data: books } = useGetBooksQuery();
+  const [search, setSearch] = useState("");
+  const [searchBooks, setSearchBooks] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (search.trim() === "") {
+      setSearchBooks(books);
+    } else {
+      const searchLower = search.toLowerCase();
+      const results = books?.filter(
+        (book) =>
+          book.title.toLowerCase().includes(searchLower) ||
+          book.author.toLowerCase().includes(searchLower)
+      );
+      setSearchBooks(results);
+    }
+  }, [books, search]);
+
+  const handleClearSearch = () => {
+    setSearch("");
+    setFilteredBooks(books);
+  };
 
   return (
     <main id="bookList">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          name="search"
+          value={search}
+          placeholder="Search..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button id="search" type="submit">
+          Search
+        </button>
+        <button id="searchClear" type="button" onClick={handleClearSearch}>
+          Clear
+        </button>
+      </form>
       <ul id="bookCard">
-        {books?.map((book) => (
-          <BookCard key={book.id} book={book} />
-        ))}
+        {searchBooks && searchBooks.length > 0 ? (
+          searchBooks?.map((book) => <BookCard key={book.id} book={book} />)
+        ) : (
+          <p>No books found.</p>
+        )}
       </ul>
     </main>
   );
