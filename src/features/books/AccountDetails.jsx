afdffd2f731110b95474abcae2f42791076f238e
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-03 21:25:37 -0600

    completed responsive design for account details component

--- src/features/books/AccountDetails.jsx
+++ src/features/books/AccountDetails.jsx
@@ -16,21 +16,19 @@ export default function AccountDetails() {
 
   return (
     <main id="accountDetails">
-      <section id="accountInfo">
-        <div id="accountEmail">
-          <h1 id="underline">Account Details</h1>
-          <p id="email">EMAIL: {regUser?.email}</p>
-        </div>
-        <div id="reservations">
-          <h2 id="underline">Reservations: </h2>
-          {reservations ? (
-            reservations.map((reservation) => (
-              <ReservedBooks key={reservation.id} reservation={reservation} />
-            ))
-          ) : (
-            <p>Nothing Reserved</p>
-          )}
-        </div>
+      <section id="accountEmail">
+        <h1 id="underline">Account Details</h1>
+        <p id="email">EMAIL: {regUser?.email}</p>
+      </section>
+      <section id="reservations">
+        <h2 id="underline">Reservations: </h2>
+        {reservations ? (
+          reservations.map((reservation) => (
+            <ReservedBooks key={reservation.id} reservation={reservation} />
+          ))
+        ) : (
+          <p>Nothing Reserved</p>
+        )}
       </section>
     </main>
   );
